d069eb7aa5e2587d1a03c2c620cb12c7c9b4c021
Tim DeCillis <timdecillis@gmail.com>
2024-01-20 14:06:41 -0800

    created fetchDetails method

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,12 +2,25 @@ export const fetchMovies = async () => {
   const key = process.env.API_KEY;
 
   const res = await fetch(
-    `http://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=terminator`
+    `http://www.omdbapi.com/?s=terminator&apikey=${key}`
   );
   const data = await res.json();
 
   if (!res.ok) {
     throw new Error("There was an error fetching the movies");
+  }
+  return data.Search;
+};
+export const fetchDetails = async (id:string) => {
+  const key = process.env.API_KEY;
+
+  const res = await fetch(
+    `http://www.omdbapi.com/?i=${id}&apikey=${key}`
+  );
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error("There was an error fetching the movie details");
   }
   return data.Search;
 };
