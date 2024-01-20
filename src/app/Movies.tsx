44fa5e9a372ca1a690d43f19e78d06e4df5c0ee0
Tim DeCillis <timdecillis@gmail.com>
2024-01-20 09:25:16 -0800

    fetched and rendered homepage movie titles

--- src/app/Movies.tsx
+++ src/app/Movies.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Movies = () => {
-  const key = process.env.NEXT_PUBLIC_KEY;
-
-  return (
-    <div>Movies</div>
-  )
+interface Movie {
+  Title: string;
+  imdbId: string;
 }
 
-export default Movies
+const Movies = async () => {
+  const key = process.env.NEXT_PUBLIC_KEY;
+  const res = await fetch(
+    `http://www.omdbapi.com/?i=tt3896198&apikey=5dc2584f&s=terminator`
+  );
+  const data = await res.json();
+  const movies: Movie[] = data.Search;
+
+  return (
+    <>
+      <h1>Movies</h1>
+      <ul>
+        {movies.map((movie: Movie) => (
+          <li key={movie.imdbId}>{movie.Title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Movies;
