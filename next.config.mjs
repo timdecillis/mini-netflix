4f16cc53032aeb16528ff1d79c9a0eff566b559a
Tim DeCillis <timdecillis@gmail.com>
2024-01-20 09:58:29 -0800

    added poster to movie

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   sassOptions: {
     includePaths: ["styles"]
+  },
+  images: {
+    domains: ['m.media-amazon.com'],
   }
 };
 
--- src/app/Movies.tsx
+++ src/app/Movies.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { fetchMovies } from "../../lib/helpers";
+import Image from "next/image";
 
 interface Movie {
   Title: string;
   imdbId: string;
+  Poster: string;
 }
 
 const Movies = async () => {
-
   const movies: Movie[] = await fetchMovies();
 
   return (
@@ -15,7 +16,16 @@ const Movies = async () => {
       <h1>Movies</h1>
       <ul>
         {movies.map((movie: Movie) => (
-          <li key={movie.imdbId}>{movie.Title}</li>
+          <li key={movie.imdbId}>
+            <h3>{movie.Title}</h3>
+            <Image
+              src={movie.Poster}
+              alt={`a poster for ${movie.Title}`}
+              width={300}
+              height={450}
+              layout="responsive"
+            ></Image>
+          </li>
         ))}
       </ul>
     </>
