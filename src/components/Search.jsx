b41e52f76f9c3c805e9664e2705a7252430c0ae0
Tim DeCillis <timdecillis@gmail.com>
2024-01-21 07:44:19 -0800

    styled home button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,18 +11,15 @@ const Search = () => {
     router.push(`/search/${input}`);
   };
   return (
-    <div >
-
-    <form  onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <input
-      className="search-input"
+        className="search-input"
         onChange={(e) => setInput(e.target.value)}
         type="text"
         placeholder="Search..."
       />
       <input disabled={!input} type="submit" />
     </form>
-    </div>
   );
 };
 
