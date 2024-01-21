725b10f4882f4774255fcb469a10f7c31e5e5e43
Tim DeCillis <timdecillis@gmail.com>
2024-01-21 08:17:19 -0800

    updated movies styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,20 +5,22 @@ import favicon from "../app/favicon.ico";
 
 const MovieCard = ({ movie }) => {
   return (
-    <a key={movie.imdbID} href={`/movie/${movie.imdbID}`}>
-      <Image
-        src={movie.Poster === "N/A" ? favicon : movie.Poster}
-        alt={
-          movie.Poster === "N/A"
-            ? "text: no image available"
-            : `a poster for ${movie.Title}`
-        }
-        width={150}
-        height={225}
-        priority
-      ></Image>
-      <h3>{movie.Title}</h3>
-    </a>
+    <div>
+      <a className="movie-card" key={movie.imdbID} href={`/movie/${movie.imdbID}`}>
+        <Image
+          src={movie.Poster === "N/A" ? favicon : movie.Poster}
+          alt={
+            movie.Poster === "N/A"
+              ? "text: no image available"
+              : `a poster for ${movie.Title}`
+          }
+          width={150}
+          height={225}
+          priority
+        ></Image>
+        <div>{movie.Title}</div>
+      </a>
+    </div>
   );
 };
 
