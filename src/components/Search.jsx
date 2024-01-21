25e4c28591d48b77a6eb0556ecd14f229a2ebace
Tim DeCillis <timdecillis@gmail.com>
2024-01-21 07:41:47 -0800

    added header styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,14 +11,18 @@ const Search = () => {
     router.push(`/search/${input}`);
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <div >
+
+    <form  onSubmit={handleSubmit}>
       <input
+      className="search-input"
         onChange={(e) => setInput(e.target.value)}
         type="text"
         placeholder="Search..."
       />
       <input disabled={!input} type="submit" />
     </form>
+    </div>
   );
 };
 
