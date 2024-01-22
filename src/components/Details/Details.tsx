4e4ba40b94ccd75d846955ba9c581e07babe0e7a
Tim DeCillis <timdecillis@gmail.com>
2024-01-22 11:53:35 -0800

    updated Details html

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -3,35 +3,34 @@ import React from "react";
 
 import { DetailsProps } from "./DetailsTypes";
 import favicon from "@/app/favicon.ico";
-import styles from './Details.module.scss'
+import styles from "./Details.module.scss";
 
 const Details = ({ details }: DetailsProps) => {
   return (
-    <div className={styles.details}>
-      <div className={styles['details-top']}>
-        <div>
-          <Image
-            src={details.Poster === "N/A" ? favicon : details.Poster}
-            alt={
-              details.Poster === "N/A"
-                ? "text: no image available"
-                : `a poster for ${details.Title}`
-            }
-            width={150}
-            height={225}
-            priority
-          ></Image>
-        </div>
-        <div className={styles['details-info']}>
+    <section className={styles.details}>
+      <section className={styles["details-top"]}>
+        <Image
+          src={details.Poster === "N/A" ? favicon : details.Poster}
+          alt={
+            details.Poster === "N/A"
+              ? "text: no image available"
+              : `a poster for ${details.Title}`
+          }
+          width={150}
+          height={225}
+          priority
+        ></Image>
+
+        <section className={styles["details-info"]}>
           <h3>{details.Title}</h3>
           <h5>Year: {details.Year}</h5>
           <h5>Rating: {details.Rated}</h5>
           <h5>Runtime: {details.Runtime}</h5>
           <h5>Written by: {details.Writer}</h5>
-        </div>
-      </div>
+        </section>
+      </section>
       <p>{details.Plot}</p>
-    </div>
+    </section>
   );
 };
 
