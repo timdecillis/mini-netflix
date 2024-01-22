bb1e164e2902b98338107be6904d185099b9fc83
Tim DeCillis <timdecillis@gmail.com>
2024-01-22 11:33:44 -0800

    added Header module

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import Image from "next/image";
 
 import logo from "@/app/logo1.png";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <header className="header">
-      <a className="home-button" href="/">
+    <header className={styles.header}>
+      <a className={styles["home-button"]} href="/">
         Home
       </a>
-      <h1 className="header-greeting" >Welcome to (mini)Netflix!</h1>
-      <div className="header-logo"  ></div>
-      <a href='/' >
-        <Image className="logo-image" src={logo} alt="the mini netflix logo"></Image>
+      <h1 className={styles["header-greeting"]}>Welcome to (mini)Netflix!</h1>
+      <a href="/">
+        <Image
+          className={styles["logo-image"]}
+          src={logo}
+          alt="the mini netflix logo"
+        ></Image>
       </a>
     </header>
   );
