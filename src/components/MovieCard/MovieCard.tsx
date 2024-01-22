0a36f78e979b607fb71658fb97ed81f455ce4fc5
Tim DeCillis <timdecillis@gmail.com>
2024-01-22 11:36:30 -0800

    added MovieCard module

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import { MovieCardProps } from "./MovieCardTypes";
 import "@/styles/main.scss";
 import favicon from "@/app/favicon.ico";
+import styles from './MovieCard.module.scss';
 
 const MovieCard = ({ movie }: MovieCardProps) => {
   return (
     <a
-      className="movie-card"
+      className={styles['movie-card']}
       key={movie.imdbID}
       href={`/movie/${movie.imdbID}`}
     >
