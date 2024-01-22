3e907cc32e392ccd6075c1d5bebabeb182d9738c
Tim DeCillis <timdecillis@gmail.com>
2024-01-22 11:57:53 -0800

    updated link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import logo from "@/app/logo1.png";
 import styles from "./Header.module.scss";
@@ -7,17 +8,17 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <a className={styles["home-button"]} href="/">
+      <Link className={styles["home-button"]} href="/">
         Home
-      </a>
+      </Link>
       <h1 className={styles["header-greeting"]}>Welcome to (mini)Netflix!</h1>
-      <a href="/">
+      <Link href="/">
         <Image
           className={styles["logo-image"]}
           src={logo}
           alt="the mini netflix logo"
         ></Image>
-      </a>
+      </Link>
     </header>
   );
 };
