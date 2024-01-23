ac41160f23fd8c756ba2b7ac2392140e51638d17
Tim DeCillis <timdecillis@gmail.com>
2024-01-22 17:13:27 -0800

    finished styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 import Header from "@/components/Header/Header";
 import Search from "@/components/Search/Search";
 import "./globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mini Netflix",
@@ -19,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         <Search />
         {children}
