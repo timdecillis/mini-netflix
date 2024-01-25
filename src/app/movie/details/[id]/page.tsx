3d334b00234282e10466ec4ce780a8d5bef51875
Tim DeCillis <timdecillis@gmail.com>
2024-01-24 19:04:49 -0800

    updated detailsPage

--- src/app/movie/details/[id]/page.tsx
+++ src/app/movie/details/[id]/page.tsx
@@ -9,11 +9,11 @@ import Details from "@/components/Details/Details";
 import { fetchDetails } from "@/lib/helpers";
 import { moviePageParams } from "../../moviePageParams";
 
-const MoviePage = async ({ params }: moviePageParams) => {
+const detailsPage = async ({ params }: moviePageParams) => {
   const { id } = params;
   const details = await fetchDetails(id);
 
   return <Details details={details} />;
 };
 
-export default MoviePage;
+export default detailsPage;
