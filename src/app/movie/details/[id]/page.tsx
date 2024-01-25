658255b36d0bcceaab61c53c1b6e4193f860d6b0
Tim DeCillis <timdecillis@gmail.com>
2024-01-24 19:18:17 -0800

    added dynamic metadata

--- src/app/movie/details/[id]/page.tsx
+++ src/app/movie/details/[id]/page.tsx
@@ -6,26 +6,18 @@ export async function generateMetadata(
   { params }: moviePageParams,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  // read route params
+
   const {id} = params;
 
-  // fetch data
   const details = await fetchDetails(id);
 
   // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || []
+  // const previousImages = (await parent).openGraph?.images || []
 
   return {
-    title: details.Title,
-    openGraph: {
-      images: ['/some-specific-page-image.jpg', ...previousImages],
-    },
+    title: details.Title
   }
 }
-
-export const metadata: Metadata = {
-  title: "Movie Details",
-};
 
 import Details from "@/components/Details/Details";
 import { fetchDetails } from "@/lib/helpers";
