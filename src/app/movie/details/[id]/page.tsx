97455e61a194f672b3178620951c32a2aa8ea672
Tim DeCillis <timdecillis@gmail.com>
2024-01-24 19:15:38 -0800

    added details

--- src/app/movie/details/[id]/page.tsx
+++ src/app/movie/details/[id]/page.tsx
@@ -1,5 +1,27 @@
 import React from "react";
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
+
+
+export async function generateMetadata(
+  { params }: moviePageParams,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const {id} = params;
+
+  // fetch data
+  const details = await fetchDetails(id);
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: details.Title,
+    openGraph: {
+      images: ['/some-specific-page-image.jpg', ...previousImages],
+    },
+  }
+}
 
 export const metadata: Metadata = {
   title: "Movie Details",
