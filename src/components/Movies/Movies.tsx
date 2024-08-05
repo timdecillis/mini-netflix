04e93825a6c2eee024931ecb942362b8e41649d8
timdecillis <timdecillis@gmail.com>
2024-08-04 21:27:14 -0700

    added Search to Movies

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { MoviesProps } from "./MoviesTypes";
 import { movieSearch } from "@/lib/omdbTypes";
 import MovieCard from "../MovieCard/MovieCard";
+import Search from "../Search/Search";
 import { fetchMovies } from "@/lib/helpers";
 import styles from "./Movies.module.scss";
 
@@ -10,15 +11,18 @@ const Movies = async ({ term }: MoviesProps) => {
   const movies: movieSearch[] = await fetchMovies(term);
 
   return (
-    <section className={styles.movies}>
-      {movies.length ? (
-        movies.map((movie: movieSearch) => (
-          <MovieCard key={movie.imdbID} movie={movie} />
-        ))
-      ) : (
-        <h1>Your search did not return any results :(</h1>
-      )}
-    </section>
+    <div>
+      <Search />
+      <section className={styles.movies}>
+        {movies.length ? (
+          movies.map((movie: movieSearch) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))
+        ) : (
+          <h1>Your search did not return any results :(</h1>
+        )}
+      </section>
+    </div>
   );
 };
 
