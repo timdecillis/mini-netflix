59bf5551881634eebe129300b82345c841eefd81
timdecillis <timdecillis@gmail.com>
2024-08-09 11:06:57 -0700

    completed update

--- src/app/account/update/page.tsx
+++ src/app/account/update/page.tsx
@@ -1,10 +1,21 @@
 import Update from "@/components/Account/Update/Update";
-import db from "@/utils/db"
+import db from "@/utils/db";
+import { revalidatePath } from "next/cache";
 
 const update = () => {
   const saveName = async (name: string) => {
-    "use server"
+    "use server";
     console.log("nameeee:", name);
+    try {
+      await db.user.create({
+        data: {
+          name,
+        },
+      });
+      revalidatePath('/account/update')
+    } catch (err) {
+      console.error(`There was an error saving to the database: ${err}`);
+    }
   };
   return <Update saveName={saveName} />;
 };
