e9532193cd6746730f4b8dcd65c9692fdd5d1a49
timdecillis <timdecillis@gmail.com>
2024-08-09 11:47:14 -0700

    deleted update path

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,4 +1,5 @@
 import db from "@/utils/db";
+import { revalidatePath } from "next/cache";
 import Link from "next/link";
 
 const getData = async () => {
@@ -6,13 +7,26 @@ const getData = async () => {
   return users
 }
 
+const saveName = async (name: string) => {
+  "use server";
+  try {
+    await db.user.create({
+      data: {
+        name,
+      },
+    });
+    revalidatePath('/account/update')
+  } catch (err) {
+    console.error(`There was an error saving to the database: ${err}`);
+  }
+};
+
 const accountPage = async () => {
   const users = await getData()
   return (
     <div>
       <h3>These are all the users</h3>
       {users.map((user, i) => <div key={i}>{user.name}</div>)}
-      <Link href="/account/update">Update your account</Link>
     </div>
   );
 };
