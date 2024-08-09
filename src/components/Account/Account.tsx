fd17926c53eb324ec3899da6d442fd28b8818e17
timdecillis <timdecillis@gmail.com>
2024-08-09 11:53:06 -0700

    updated props

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -4,7 +4,7 @@ import { SyntheticEvent, useState } from "react";
 import Update from "./Update/Update";
 
 const Account = ({
-  saveName,
+  saveName, users
 }: {
   saveName: (name: string) => void;
   users: { id: string; name: string }[];
@@ -12,7 +12,7 @@ const Account = ({
   const [updateOpen, setupdateOpen] = useState(false);
   return (
     <>
-      <button onClick={() => setupdateOpen(true)} >Add user</button>
+      <button onClick={() => setupdateOpen(true)}>Add user</button>
       <h3>These are all the users</h3>
       {users.map((user, i) => (
         <div key={i}>{user.name}</div>
