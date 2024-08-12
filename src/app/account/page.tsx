712cc703f2ba254c37e51ac4e0c5076eeb243c86
timdecillis <timdecillis@gmail.com>
2024-08-12 13:50:25 -0700

    moved getData and saveUser to helpers

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,29 +1,9 @@
 import Account from "@/components/Account/Account";
-import db from "@/utils/db";
-import { revalidatePath } from "next/cache";
+import { saveName, getData } from "@/lib/helpers";
 
 export type User = {
   id: string;
   name: string;
-};
-
-const getData = async () => {
-  const users = await db.user.findMany({});
-  return users;
-};
-
-const saveName = async (name: string) => {
-  "use server";
-  try {
-    await db.user.create({
-      data: {
-        name,
-      },
-    });
-    revalidatePath("/account");
-  } catch (err) {
-    console.error(`There was an error saving to the database: ${err}`);
-  }
 };
 
 const accountPage = async () => {
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,6 +1,10 @@
+import { revalidatePath } from "next/cache";
 import { MovieSearchProps, DetailsProps } from "./omdbTypes";
+import db from "@/utils/db";
 
-export const fetchMovies = async (term: string): Promise<MovieSearchProps[]> => {
+export const fetchMovies = async (
+  term: string
+): Promise<MovieSearchProps[]> => {
   const key = process.env.API_KEY;
 
   const res = await fetch(`http://www.omdbapi.com/?s=${term}&apikey=${key}`);
@@ -23,4 +27,23 @@ export const fetchDetails = async (id: string): Promise<DetailsProps> => {
     throw new Error("There was an error fetching the movie details");
   }
   return data || [];
+};
+
+export const getData = async () => {
+  const users = await db.user.findMany({});
+  return users;
+};
+
+export const saveName = async (name: string) => {
+  "use server";
+  try {
+    await db.user.create({
+      data: {
+        name,
+      },
+    });
+    revalidatePath("/account");
+  } catch (err) {
+    console.error(`There was an error saving to the database: ${err}`);
+  }
 };
